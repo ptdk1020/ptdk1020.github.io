373e965cc6a7c85cde1dd391f2944853158948f2
ptdk1020 <66972470+ptdk1020@users.noreply.github.com>
2023-06-13 18:08:04 -0400

    aboutme under construction

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function AboutMe() {
   return (
     <div>
-      <h2>This page is (also) under construction</h2>
+      <h2>This page is under construction</h2>
     </div>
   )
 }
