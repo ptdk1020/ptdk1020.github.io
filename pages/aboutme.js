9b9f8ee44f2c056a5a3ba8988c3f209e5f598e20
ptdk1020 <66972470+ptdk1020@users.noreply.github.com>
2023-07-04 12:20:13 -0400

    fix typo in aboutme

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
     <div>
       <h2>Welcome</h2>
       <p>Hi there, I'm Khoa. Thanks for stopping by. This site is a place for me to write about my projects, I'd like to keep it fun and informative.</p>
-      <p><i>This page is still be constructed</i></p>
+      <p><i>This page is still being constructed</i></p>
     </div>
   )
 }
